66f380d42c879b2b764935a60c750de46903d1c3
farzan190 <farzanxavier@gmail.com>
2024-06-07 17:33:24 +0530

    added todofiltercontrol logic

--- myapp/src/components/TodoFilterControl.js
+++ myapp/src/components/TodoFilterControl.js
@@ -1,5 +1,30 @@
 const TodoFilterControl = ({ filterStatus, setFilterStatus }) => {
-    
-};
-
-export default TodoFilterControl;
+    const handleClick = (status) => {
+      setFilterStatus(status);
+    };
+  
+    return (
+      <div className="control-btn group">
+        <button
+          className={filterStatus === "all" ? "btn active" : "btn"}
+          onClick={() => handleClick("all")}
+        >
+          All
+        </button>
+        <button
+          className={filterStatus === "active" ? "btn active" : "btn"}
+          onClick={() => handleClick("active")}
+        >
+          Active
+        </button>
+        <button
+          className={filterStatus === "completed" ? "btn active" : "btn"}
+          onClick={() => handleClick("completed")}
+        >
+          Completed
+        </button>
+      </div>
+    );
+  };
+  
+  export default TodoFilterControl;
